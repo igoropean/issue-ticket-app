3d6ce95cf831ba0f3457064e940c1ce393b8e664
igoropean <kechag@gmail.com>
2026-04-25 03:34:14 +0800

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,1 +1,6 @@
-const API_URL="https://script.google.com/macros/s/AKfycbxvfiQewDDaBOiwvRHH6OOKFva-rP5em8Lw0Pga2WYcCw90iBppaxQm9b-1Tbvlvxqk/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbxvfiQewDDaBOiwvRHH6OOKFva-rP5em8Lw0Pga2WYcCw90iBppaxQm9b-1Tbvlvxqk/exec";
+const DB_NAME = "issue_ticket_app_db_v2";
+const DB_STORE = "pending_tickets";
+const SESSION_KEY = "issue_ticket_session_v2";
+const MAX_PHOTOS = 3;
+const APP_SCOPE = "/issue-ticket-app/";
