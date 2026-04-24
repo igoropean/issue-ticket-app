480cb79f69d59344c3593b00a9e3359f4dc541c5
igoropean <kechag@gmail.com>
2026-04-25 03:50:18 +0800

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,4 +1,4 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbxvfiQewDDaBOiwvRHH6OOKFva-rP5em8Lw0Pga2WYcCw90iBppaxQm9b-1Tbvlvxqk/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbxvTwuoegZQ42RqET7onoPY7KGQsdAWm44-h9uS7a6MNWktg7-tlahI122w8ASr-6Rd/exec";
 const DB_NAME = "issue_ticket_app_db_v2";
 const DB_STORE = "pending_tickets";
 const SESSION_KEY = "issue_ticket_session_v2";
