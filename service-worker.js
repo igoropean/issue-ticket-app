03b735fe62ca08eda6ceb1c763c01fe8826efadc
igoropean <kechag@gmail.com>
2026-04-25 00:11:40 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,30 @@
-const CACHE='issue-v2';
+const CACHE_NAME = "issue-v3";
 
-self.addEventListener('install',e=>{
-e.waitUntil(
-caches.open(CACHE).then(c=>c.addAll([
-'/issue-ticket-app/',
-'/issue-ticket-app/index.html'
-]))
-);
+const FILES = [
+  "/issue-ticket-app/",
+  "/issue-ticket-app/index.html",
+  "/issue-ticket-app/manifest.json"
+];
+
+self.addEventListener("install", e => {
+  self.skipWaiting();
+
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+  );
 });
 
-self.addEventListener('fetch',e=>{
-e.respondWith(
-fetch(e.request).catch(()=>caches.match(e.request))
-);
+self.addEventListener("activate", e => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", e => {
+
+  if (e.request.method !== "GET") return;
+
+  e.respondWith(
+    fetch(e.request)
+      .then(res => res)
+      .catch(() => caches.match(e.request))
+  );
 });
